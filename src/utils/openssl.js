18fa25f234394dc7fdf1cb5701c77d524ffef733
David Mart <david.mtlopez@gmail.com>
2018-02-23 09:58:17 -0600

    3.3.19

--- src/utils/openssl.js
+++ src/utils/openssl.js
@@ -6,8 +6,8 @@ const path = require('path');
 
 
 exports.decryptPKCS8PrivateKey = (options) => {
+    process.env.OPENSSL_CONF =  path.join(options.openssl_path, 'openssl.cfg');
     let openssl_bin = path.join(options.openssl_path || '', 'openssl');
-
     try {
         let ans = execFileSync(openssl_bin, ['pkcs8', '-inform', 'DER', '-in', `${options.in}`, '-outform', 'PEM', '-passin', `pass:${options.pass}`]);
         return ans.toString('utf8');
